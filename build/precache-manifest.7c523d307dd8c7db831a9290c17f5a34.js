db8226bbc00980a755ba732027cededd2f4951f5
Andrey Shulzhenko <imnot.xen@gmail.com>
2020-07-26 22:35:54 +0100

    Updating build to latest

---
+++ build/precache-manifest.7c523d307dd8c7db831a9290c17f5a34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43d97c4f85418b6dfec5d686e5e15f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ca696193e1a6cc9a848",
+    "url": "/static/css/main.928a2bfa.chunk.css"
+  },
+  {
+    "revision": "3012413c24687de0a1fc",
+    "url": "/static/js/2.c2e98501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2e98501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca696193e1a6cc9a848",
+    "url": "/static/js/main.163763af.chunk.js"
+  },
+  {
+    "revision": "5c6e6548b587f727f88c",
+    "url": "/static/js/runtime-main.e55f7b33.js"
+  },
+  {
+    "revision": "5795aef1c74c31c0df151edd48ef3d06",
+    "url": "/static/media/MA_Seagull_Black.5795aef1.png"
+  },
+  {
+    "revision": "e5ed1398f1be63d4f05508d06b484792",
+    "url": "/static/media/eberhard-grossgasteiger-1096581-unsplash.e5ed1398.jpg"
+  },
+  {
+    "revision": "aedd9f8a385eaac92847966f2f7b4463",
+    "url": "/static/media/eberhard-grossgasteiger-1435104-unsplash.aedd9f8a.jpg"
+  },
+  {
+    "revision": "51c529a082c9e33fc341f783d8a80cc6",
+    "url": "/static/media/eberhard-grossgasteiger-1441879-unsplash.51c529a0.jpg"
+  },
+  {
+    "revision": "b906e7d2f41686632b95db9de8df0ae6",
+    "url": "/static/media/iconmonstr-instagram-14-240.b906e7d2.png"
+  },
+  {
+    "revision": "e063dc2f9db4e30c412d6671083b9fd5",
+    "url": "/static/media/iconmonstr-twitter-5-240.e063dc2f.png"
+  }
+]);
